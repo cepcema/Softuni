c31f315f7e8a858a0a58531be8646bebf9445880
cepcema <49753804+cepcema@users.noreply.github.com>
2019-06-21 20:00:25 +0300

    Update solution.js

--- 02_Find-Ascii-Equivalent/solution.js
+++ 02_Find-Ascii-Equivalent/solution.js
@@ -1,4 +1,19 @@
 function solve() {
+  let text = document.getElementById("text").value;
+  let parts = text.split(` `);
+  let words = ``;
+  let numbers = ` `;
 
-  //TODO...
+  parts.forEach(element => {
+    if (isNaN(+element)) {
+      //this is not a number
+      words += [...element].map(ch => ch.charCodeAt(0)).join(` `);
+      words += `/n`;
+    } else {
+      
+     numbers += String.fromCharCode(+element)
+    }
+  });
+  console.log(words)
+  console.log(numbers)
 }
