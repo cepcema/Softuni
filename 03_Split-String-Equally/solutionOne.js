3b702ce6c6cee32d0d54b9999b0ea4e3f4c4fa84
cepcema <49753804+cepcema@users.noreply.github.com>
2019-06-21 20:28:28 +0300

    Update solutionOne.js

--- 03_Split-String-Equally/solutionOne.js
+++ 03_Split-String-Equally/solutionOne.js
@@ -1,3 +1,9 @@
-function solve() {
-    //TODO...
+function solve () {
+  let str = document.querySelector('#text').value
+  let num = document.querySelector('#number').value
+  let result = str.match(new RegExp(`.{1,${num}}`, 'g'))
+  let lastElement = result.pop()
+  lastElement += str.slice(0, num - lastElement.length)
+  result.push(lastElement)
+  document.querySelector('#result').textContent = result.join(' ')
 }
