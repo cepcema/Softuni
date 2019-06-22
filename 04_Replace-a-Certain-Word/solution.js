3044c15b986f8d1c3481d76d65165f8096bde709
cepcema <49753804+cepcema@users.noreply.github.com>
2019-06-22 20:21:50 +0300

    Update solution.js

--- 04_Replace-a-Certain-Word/solution.js
+++ 04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,13 @@
-function solve() {
+function solve () {
+  let arr = JSON.parse(document.querySelector('text').value)
+  let newWord = document.querySelector('word').value
 
-    //TODO...
+  let result = document.querySelector('#result')
+  let oldWord = arr[0].split(' ')[2]
+
+  arr.forEach(line => {
+    let p = document.createElement('p')
+    p.textContent = line.replace(new RegExp(oldWord, 'i'), newWord)
+    result.appendChild(p)
+  })
 }
